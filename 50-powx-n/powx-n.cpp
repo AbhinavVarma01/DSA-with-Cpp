0d94c60cf1455b195bdef51473d3cb9f16a256c1
Abhinav Varma <abhinavvar2006@gmail.com>
2026-02-24 22:59:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (43.91%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+    double power(double x, long long n){
+        if(n==0) return 1;
+        if(n%2==1){
+            return x * power(x, n-1);
+        }
+        return power(x*x, n/2);
+    }
+
+    double myPow(double x, int n) {
+        long long num = n;
+        if(num<0){
+            return ( 1 / power(x, -num));
+        }
+        return power(x, num);
+    }
+};
