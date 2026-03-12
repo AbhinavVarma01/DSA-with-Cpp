d87bf4705b3e4fbf470dafdbed83fe31c418deba
Abhinav Varma <abhinavvar2006@gmail.com>
2026-03-12 20:24:06 +0530

    Time: 85 ms (72.08%) | Memory: 215.3 MB (97.97%) - LeetSync

--- 3902-maximize-spanning-tree-stability-with-upgrades/maximize-spanning-tree-stability-with-upgrades.cpp
+++ 3902-maximize-spanning-tree-stability-with-upgrades/maximize-spanning-tree-stability-with-upgrades.cpp
@@ -1,39 +1,47 @@
 class Solution {
 public:
-    vector<int> p;
-    int find(int x){ return p[x]==x?x:p[x]=find(p[x]); }
-    bool unite(int a,int b){
-        a=find(a); b=find(b);
-        if(a==b) return 0;
-        p[b]=a; return 1;
+
+vector<int> parent;
+
+int find(int x){
+    if(parent[x]==x) return x;
+    return parent[x]=find(parent[x]);
+}
+
+bool unite(int a,int b){
+    a=find(a); 
+    b=find(b);
+    if(a==b) return false;
+    parent[b]=a;
+    return true;
+}
+
+bool ok(int n, vector<vector<int>>& e, int k, int x){
+    iota(parent.begin(), parent.end(), 0);
+    int used=0, up=0;
+
+    for(auto &v:e)
+        if(v[3] && (v[2]<x || !unite(v[0],v[1]))) return 0;
+        else if(v[3]) used++;
+
+    for(auto &v:e)
+        if(!v[3] && v[2]>=x && unite(v[0],v[1])) used++;
+
+    for(auto &v:e)
+        if(!v[3] && v[2]<x && 2*v[2]>=x && up<k && unite(v[0],v[1]))
+            up++, used++;
+
+    return used==n-1;
+}
+
+int maxStability(int n, vector<vector<int>>& e, int k) {
+    parent.resize(n);
+    int l=1, r=200000, ans=-1;
+    while(l<=r){
+        int m=(l+r)/2;
+        if(ok(n,e,k,m)) ans=m, l=m+1;
+        else r=m-1;
     }
-
-    bool ok(int n, vector<vector<int>>& e, int k, int x){
-        iota(p.begin(), p.end(), 0);
-        int used=0, up=0;
-
-        for(auto &v:e)
-            if(v[3] && (v[2]<x || !unite(v[0],v[1]))) return 0;
-            else if(v[3]) used++;
-
-        for(auto &v:e)
-            if(!v[3] && v[2]>=x && unite(v[0],v[1])) used++;
-
-        for(auto &v:e)
-            if(!v[3] && v[2]<x && 2*v[2]>=x && up<k && unite(v[0],v[1]))
-                up++, used++;
-
-        return used==n-1;
-    }
-
-    int maxStability(int n, vector<vector<int>>& e, int k) {
-        p.resize(n);
-        int l=1, r=200000, ans=-1;
-        while(l<=r){
-            int m=(l+r)/2;
-            if(ok(n,e,k,m)) ans=m, l=m+1;
-            else r=m-1;
-        }
-        return ans;
-    }
+    return ans;
+}
 };
