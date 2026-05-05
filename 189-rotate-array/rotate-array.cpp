3283d1a4811a89699576f202227d20bc3b2d8535
Abhinav Varma <abhinavvar2006@gmail.com>
2026-05-05 19:29:08 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (40.44%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,7 +1,9 @@
+using namespace std;
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
+        if(n==0) return;
         k = k % n;
         std::rotate(nums.begin(), nums.end()-k, nums.end());
     }
