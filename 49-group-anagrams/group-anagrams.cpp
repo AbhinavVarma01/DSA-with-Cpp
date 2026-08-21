e300c937579a1d8de2e88b18065e0a739e4c56f8
Abhinav Varma <abhinavvar2006@gmail.com>
2026-08-21 15:43:02 +0530

    Time: 73 ms (7.66%) | Memory: 34.2 MB (11.03%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -5,11 +5,9 @@ public:
 
         for(string s : strs){
             int freq[26] = {0};
-
             for(char c : s){
                 freq[c - 'a']++;
             }
-
             string key = "";
             for(int i=0; i<26; i++){
                 key+= to_string(freq[i]) +"#";
